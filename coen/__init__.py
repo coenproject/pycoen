727700281c34c1427793e7e34ef29b8f3c3a78d4
Madhavan Raja <madhavanraja99@gmail.com>
2023-01-14 13:17:32 -0700

    Document creation works

--- coen/__init__.py
+++ coen/__init__.py
@@ -1,10 +1,13 @@
 import os
+import json
 
 
 class Coen:
     def __init__(self, project_directory):
         self.project_name = os.path.basename(project_directory)
         self.project_directory = project_directory
+
+        self.content = ""
 
         if not os.path.exists(self.project_directory):
             os.makedirs(self.project_directory)
@@ -49,5 +52,54 @@ class Coen:
             self.project_directory, "src", "main.coen")
         return main_file_path
 
+    def get_tex_file_path(self):
+        tex_file_path = os.path.join(
+            self.project_directory, "intermediates", "main.tex")
+        return tex_file_path
+
+    def execute_command(self):
+        match self.current_command.lower():
+            case "section":
+                self.content += f"\\section{{{' '.join(self.arguments)}}}\n"
+            case "subsection":
+                self.content += f"\\subsection{{{' '.join(self.arguments)}}}\n"
+            case _:
+                print(f"Unknown Command: {self.current_command}")
+
+    def build_content(self):
+        with open(self.get_main_file_path(), "r") as main_file:
+            lines = main_file.readlines()
+            for line in lines:
+                self.current_statement = line
+
+                match self.current_statement[0]:
+                    case "!":
+                        self.current_command = self.current_statement.split()[
+                            0][1:]
+                        self.arguments = self.current_statement.split()[1:]
+                        self.execute_command()
+                    case _:
+                        self.content += self.current_statement
+
     def build(self):
-        pass
+        self.build_tex()
+
+    def build_tex(self):
+        self.build_content()
+        info = json.load(open(self.get_coen_file_path()))
+
+        with open(self.get_tex_file_path(), "w") as tex_file:
+            tex_file.writelines(
+                [
+                    f"\\documentclass{{article}}\n",
+                    "\n",
+                    f"\\title{{{info.get('name', '')}}}\n",
+                    f"\\author{{{info.get('author', '')}}}\n",
+                    "\n",
+                    f"\\begin{{document}}\n",
+                    f"\n",
+                    self.content,
+                    f"\n",
+                    f"\\end{{document}}\n",
+                ]
+            )
