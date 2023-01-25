61cf6b183db1757205d546f86f986b96c7b802f8
Madhavan Raja <madhavanraja99@gmail.com>
2023-01-25 10:42:37 -0700

    Importing files in real time

--- coen/__init__.py
+++ coen/__init__.py
@@ -53,61 +53,17 @@ class Coen:
                 self.variables[self.arguments[0]] = ' '.join(
                     self.arguments[1:])
             case "import":
-                pass
+                import_file_path = os.path.join(
+                    os.path.dirname(self.current_file_path), ' '.join(self.arguments))
+                self.build_content(import_file_path)
             case _:
                 print(f"Unknown Command: {self.current_command}")
 
-    def build_content(self):
-        lines = self.coen_contents.split("\n")
-        for line in lines:
-            self.current_statement = line.strip() + "\n"
-
-            match self.current_statement[0]:
-                case "!":
-                    self.current_command = self.current_statement.split()[
-                        0][1:]
-                    self.arguments = self.current_statement.split()[1:]
-                    self.execute_command()
-                case _:
-                    self.content += self.current_statement
-
-    def build(self):
-        self.coen_contents = self.generate_combined_contents(
-            self.get_main_file_path())
-
-        print(self.coen_contents)
-
-        self.build_tex()
-
-        for _ in range(2):
-            command = f"cd {self.get_intermediates_directory_path()} && pdflatex main.tex"
-            os.system(command)
-
-    def generate_combined_contents(self, path):
-        current_content = ""
-        with open(path, "r") as main_file:
-            lines = main_file.readlines()
-
+    def build_content(self, file_path):
+        with open(file_path, "r") as current_file:
+            lines = current_file.readlines()
             for line in lines:
-                self.current_statement = line.strip() + "\n"
-
-                if self.current_statement.startswith("!import "):
-                    self.current_command = self.current_statement.split()[
-                        0][1:]
-                    self.arguments = self.current_statement.split()[1:]
-                    import_file_path = os.path.join(
-                        os.path.dirname(path), ' '.join(self.arguments))
-                    current_content += self.generate_combined_contents(
-                        import_file_path)
-                else:
-                    current_content += self.current_statement
-        return current_content
-
-    def compile(self, path):
-        with open(path, "r") as main_file:
-            lines = main_file.readlines()
-
-            for line in lines:
+                self.current_file_path = file_path
                 self.current_statement = line.strip() + "\n"
 
                 match self.current_statement[0]:
@@ -119,26 +75,59 @@ class Coen:
                     case _:
                         self.content += self.current_statement
 
-                    # if self.current_statement.startswith("!import"):
-                    #     self.current_command = self.current_statement.split()[
-                    #         0][1:]
-                    #     self.arguments = self.current_statement.split()[1:]
-                    #     import_file_path = os.path.join(
-                    #         os.path.dirname(path), ' '.join(self.arguments))
-                    #     current_content += self.generate_combined_contents(
-                    #         import_file_path)
-                    # else:
-                    #     current_content += self.current_statement
+    def build(self):
+        # self.coen_contents = self.generate_combined_contents(
+        #     self.get_main_file_path())
+
+        self.build_tex()
+
+        for _ in range(2):
+            command = f"cd {self.get_intermediates_directory_path()} && pdflatex main.tex"
+            os.system(command)
+
+    # def generate_combined_contents(self, path):
+    #     current_content = ""
+    #     with open(path, "r") as main_file:
+    #         lines = main_file.readlines()
+
+    #         for line in lines:
+    #             self.current_statement = line.strip() + "\n"
+
+    #             if self.current_statement.startswith("!import "):
+    #                 self.current_command = self.current_statement.split()[
+    #                     0][1:]
+    #                 self.arguments = self.current_statement.split()[1:]
+    #                 import_file_path = os.path.join(
+    #                     os.path.dirname(path), ' '.join(self.arguments))
+    #                 current_content += self.generate_combined_contents(
+    #                     import_file_path)
+    #             else:
+    #                 current_content += self.current_statement
+    #     return current_content
+
+    # def compile(self, path):
+    #     with open(path, "r") as main_file:
+    #         lines = main_file.readlines()
+
+    #         for line in lines:
+    #             self.current_statement = line.strip() + "\n"
+
+    #             match self.current_statement[0]:
+    #                 case "!":
+    #                     self.current_command = self.current_statement.split()[
+    #                         0][1:]
+    #                     self.arguments = self.current_statement.split()[1:]
+    #                     self.execute_command()
+    #                 case _:
+    #                     self.content += self.current_statement
 
     def build_tex(self):
-        self.build_content()
+        self.build_content(self.get_main_file_path())
 
         template_file = open(self.variables.get("TEMPLATE"), "r")
         tex_content = template_file.read()
 
         tex_content = tex_content.replace("$CONTENT", self.content)
-
-        print(self.variables)
 
         for key, value in self.variables.items():
             tex_content = tex_content.replace("$" + key, value)
